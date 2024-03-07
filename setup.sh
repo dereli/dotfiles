e5c97b23cf453930d30d322649697831c4e307ed
Zubeyr Dereli <dereli@gmail.com>
2024-03-07 20:21:23 +0100

    improve setup and vscode fonts

--- setup.sh
+++ setup.sh
@@ -6,4 +6,6 @@ mkdir -p ~/.ssh ~/.zsh ~/.config
 [ -f ~/.gitconfig ] || ln -s $cwd/gitconfig ~/.gitconfig
 [ -f ~/.ssh/config ] || ln -s $cwd/sshconfig ~/.ssh/config
 [ -f ~/.config/node.pem ] || touch ~/.config/node.pem
+[ -f ~/Library/Application\ Support/Code/User/settings.json ] || ln -s $cwd/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+[ -f ~/Library/Application\ Support/Code/User/keybindings.json ] || ln -s $cwd/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 curl -s https://git.kernel.org/pub/scm/git/git.git/plain/contrib/completion/git-completion.bash -o ~/.zsh/git-completion.bash
