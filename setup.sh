41a9726772e1cc348113f3fd395bee37b4af5ae1
Zubeyr Dereli <dereli@gmail.com>
2024-03-07 19:38:47 +0100

    improve setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cwd=`dirname $(realpath "$0")`
-[ -f ~/.zshrc ] || ln -s $cwd/zshrc ~/.zshrc
+mkdir -p ~/.ssh ~/.zsh ~/.config
+[ -f ~/.zshenv ] || echo "ZDOTDIR=~/.zsh" > ~/.zshenv
+[ -f ~/.zsh/.zshrc ] || ln -s $cwd/zshrc ~/.zsh/.zshrc
 [ -f ~/.gitconfig ] || ln -s $cwd/gitconfig ~/.gitconfig
 [ -f ~/.ssh/config ] || ln -s $cwd/sshconfig ~/.ssh/config
-mkdir -p ~/.zsh
-curl https://git.kernel.org/pub/scm/git/git.git/plain/contrib/completion/git-completion.bash -o ~/.zsh/git-completion.bash
+[ -f ~/.config/node.pem ] || touch ~/.config/node.pem
+curl -s https://git.kernel.org/pub/scm/git/git.git/plain/contrib/completion/git-completion.bash -o ~/.zsh/git-completion.bash
